2505cf81f2034c0b1ee8d3d1bab35e5d59c7677a
nandoIT <nando@competa.com>
2017-02-17 12:40:21 +0100

    Changed it to module pattern

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("breakout");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
 var x = canvas.width/2;
@@ -6,7 +6,7 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var paddleHeight = 10;
-var paddleWidth = 75;
+var paddleWidth = 65;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
@@ -47,8 +47,6 @@ function drawPaddle(){
   ctx.fill();
   ctx.closePath();
 }
-
-
 
 function draw(){
   ctx.clearRect(0,0, canvas.width, canvas.height);
---
+++ javascript/move-ball.js
@@ -0,0 +1,18 @@
+var MoveBall = (function(){
+  return{
+  moveBall : function(){
+    Init.breakout.clearRect(0,0, Init.canvas.width, Init.canvas.height);
+    Elements.drawElements();
+    if(Init.x + Init.dx > Init.canvas.width-Init.ballRadius || Init.x + Init.dx < Init.ballRadius) {
+        Init.dx =- Init.dx;
+    }
+
+    if(Init.y + Init.dy > Init.canvas.height-Init.ballRadius || Init.y + Init.dy < Init.ballRadius){
+      Init.dy =- Init.dy;
+    }
+    Init.x += Init.dx;
+    Init.y += Init.dy;
+  }
+  
+}
+})();
