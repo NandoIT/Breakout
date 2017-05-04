365c837e7c07266461c9111c7495da86397582a1
nandoIT <nando@competa.com>
2017-05-04 11:47:59 +0200

    Splitted up the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,4 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      options: {
-        reporter: require('jshint-stylish')
-      },
-      build: ['Grunfile.js', 'src/**/*.js']
-    },
-  	uglify: {
-        options: {
-          banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
-        },
-        build: {
-          files: {
-            'dist/js/magic.min.js': ['src/**/*.js']
-          }
-        }
-      },
-    watch: {
-      scripts: {
-        files: 'src/**/*.js', tasks: ['jshint', 'uglify']
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', ['jshint', 'uglify', 'watch']);
-
+module.exports = grunt => {
+	// load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+	require('load-grunt-config')(grunt);
 };
